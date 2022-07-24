c0132e79ee2a8131794eb30af96b88be566da4f2
Ramon Torres <RTorres5318@gmail.com>
2022-07-24 11:14:50 -0400

    Added comments for controllers files

--- server/controllers/Photos.js
+++ server/controllers/Photos.js
@@ -1,41 +1,50 @@
+/*==================================================
+server/controllers/Photos.js
+
+Controls all of the Photo actions a User can take when using the backend API.
+Gets imported and used by routes/Photos.js
+==================================================*/
+
+// Import modules
 import ash from 'express-async-handler';
 import mongoose from 'mongoose';
 
+// Import Photo database model
 import Photo from '../database/models/Photo.js';
 
 export const getPhoto = ash(async (req, res) => {
-    const { id } = req.params;
+    const { id } = req.params; // Destructure data from req.params
 
-    const photo = await Photo.findById(id);
+    const photo = await Photo.findById(id); // Query database for photo using the photo_id
 
-    res.status(200).json(photo);
+    res.status(200).json(photo); // Return photo if found
 });
 
 export const getPhotos = ash(async (req, res) => {
-    const { page } = req.query;
+    const { page } = req.query; // Destructure data from req.query
 
-    const LIMIT = 8;
-    const startIndex = (Number(page) - 1) * LIMIT;
-    const total = await Photo.countDocuments({});
+    const LIMIT = 8; // Setting the maximum amount of photos that are viewable per page
+    const startIndex = (Number(page) - 1) * LIMIT; // Sets where the backend will start to query the database
+    const total = await Photo.countDocuments({}); // Query database and return total amount of photos
 
-    const photos = await Photo.find().sort({id: -1}).limit(LIMIT).skip(startIndex);
-    res.status(200).json({ data: photos, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) });
+    const photos = await Photo.find().sort({id: -1}).limit(LIMIT).skip(startIndex); // Query database to display no more than the LIMIT of photos after the startIndex
+    res.status(200).json({ data: photos, currentPage: Number(page), numberOfPages: Math.ceil(total / LIMIT) }); // Return photos on the current page
 });
 
 export const getPhotosBySearch = ash(async (req, res) => {
-    const { searchQuery, tags } = req.query;
+    const { searchQuery, tags } = req.query; // Destructure data from req.query
 
-    const title = new RegExp(searchQuery, "i");
+    const title = new RegExp(searchQuery, "i"); // Sets the values in the searchQuery to all be read as lower-case ("i" flag stands for ignore case)
 
-    const photos = await Photo.find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] });
+    const photos = await Photo.find({ $or: [{ title }, { tags: { $in: tags.split(",") } }] }); // Query database using the given search parameters
 
-    res.status(200).json({ data: photos });
+    res.status(200).json({ data: photos }); // Return the photos if found
 });
 
 export const addPhoto = ash(async (req, res) => {
-    const { title, caption, tags, selectedFile } = req.body;
+    const { title, caption, tags, selectedFile } = req.body; // Destructure data from req.body
 
-    const fixedTags = tags.map((tag) => {
+    const fixedTags = tags.map((tag) => { // Fixes tags entered by user so that if the first character in a tag is a blank space it is removed
         if (tag[0] === " ") {
             return tag.substr(1);
         } else {
@@ -45,17 +54,18 @@ export const addPhoto = ash(async (req, res) => {
 
     const newPhoto = new Photo({ title, caption, tags: fixedTags, selectedFile, creator: req.userId, created: new Date().toISOString() });
 
+    // Save new photo to database
     await newPhoto.save();
-    res.status(201).json(newPhoto);
+    res.status(201).json(newPhoto); // Return the newly added photo
 });
 
 export const updatePhoto = ash(async (req, res) => {
-    const { id: _id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No photo with that ID");
+    const { id: _id } = req.params; // Destructure the photo id from req.params
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No photo with that ID"); // Check to ensure valid photo is chosen
 
-    const { title, caption, tags, creator, created } = req.body;
+    const { title, caption, tags, creator, created } = req.body; //Destructure data from req.body
 
-    const fixedTags = tags.map((tag) => {
+    const fixedTags = tags.map((tag) => { // Fixes tags entered by user so that if the first character in a tag is a blank space it is removed
         if (tag[0] === " ") {
             return tag.substr(1);
         } else {
@@ -63,16 +73,18 @@ export const updatePhoto = ash(async (req, res) => {
         }
     });
 
+    // Update selected photo with entered data
     const updatedPhoto = await Photo.findByIdAndUpdate(_id, { title, caption, tags: fixedTags, creator, created, _id }, { new: true });
 
-    res.status(201).json(updatedPhoto);
+    res.status(201).json(updatedPhoto); // Return the updated photo
 });
 
 export const deletePhoto = ash(async (req, res) => {
-    const { id: _id } = req.params;
-    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No photo with that ID");
+    const { id: _id } = req.params; // Destructure the photo id from req.params
+    if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send("No photo with that ID"); // Check to ensure valid photo is chosen
 
+    // Delete photo from database
     const deletedPhoto = await Photo.findByIdAndRemove(_id);
 
-    res.status(200).json({ message: "Photo successfully deleted" });
+    res.status(200).json({ message: "Photo successfully deleted" }); // Return success message
 });
--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,37 +1,49 @@
+/*==================================================
+server/controllers/User.js
+
+Controls all of the actions a User can take when using the backend API.
+Gets imported and used by routes/Users.js
+==================================================*/
+
+// Import modules
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
 import ash from 'express-async-handler';
 
+// Import User database model
 import User from '../database/models/User.js';
 
+
 export const signIn = ash(async (req, res) => {
-    const { email, password } = req.body;
+    const { email, password } = req.body; // Destructure data from req.body
 
-    const existingUser = await User.findOne({ email });
-    if (!existingUser) return res.status(400).json({ message: "Incorrect email/password combination"});
+    const existingUser = await User.findOne({ email }); // Query database to check if email already exists
+    if (!existingUser) return res.status(400).json({ message: "Incorrect email/password combination"}); // If not, send error
 
-    const authenticatePassword = await bcrypt.compare(password, existingUser.password);
-    if (!authenticatePassword) return res.status(400).json({message: "Incorrect email/password combination"});
+    const authenticatePassword = await bcrypt.compare(password, existingUser.password); // If user is found, verify login information
+    if (!authenticatePassword) return res.status(400).json({message: "Incorrect email/password combination"}); // Send error if verification failed
 
     const token = jwt.sign({ email: existingUser.email, id: existingUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
 
-    res.status(200).json({ result: existingUser, token });
+    res.status(200).json({ result: existingUser, token }); // User is successfully logged in
 });
 
 export const signUp = ash(async (req, res) => {
-    const { firstName, lastName, email, password, confirmPassword } = req.body;
+    const { firstName, lastName, email, password, confirmPassword } = req.body; // Destructure data from req.body
+
+    // Fixing user input to ensure first & last name begin with capital letters
     const fName = firstName.substr(0, 1).toUpperCase() + firstName.substr(1);
     const lName = lastName.substr(0, 1).toUpperCase() + lastName.substr(1);
 
-    const existingUser = await User.findOne({ email });
-    if (existingUser) return res.status(400).json({ message: "That email is already registered" });
-    if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" });
+    const existingUser = await User.findOne({ email }); // Query database to check if email already exists
+    if (existingUser) return res.status(400).json({ message: "That email is already registered" }); // If so, send error
+    if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" }); // Password correctness check
 
-    const hashedPassword = await bcrypt.hash(password, 12);
+    const hashedPassword = await bcrypt.hash(password, 12); // Take entered password and create hash using bcrypt
 
-    const newUser = await User.create({ email, password: hashedPassword, name: `${fName} ${lName}` });
+    const newUser = await User.create({ email, password: hashedPassword, name: `${fName} ${lName}` }); // Add new user to database
 
-    const token = jwt.sign({ email: newUser.email, id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ email: newUser.email, id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" }); // JWT_SECRET can be set in .env file
 
-    res.status(200).json({ result: newUser, token });
+    res.status(200).json({ result: newUser, token }); // User was successfully signed up
 });
