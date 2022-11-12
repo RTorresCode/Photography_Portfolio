98c868d6e133fa35f4c13a73e55331ee539a8359
Ramon Torres <RTorres5318@gmail.com>
2022-11-12 15:31:35 -0500

    Restricted new users from signing up.

--- server/controllers/User.js
+++ server/controllers/User.js
@@ -28,22 +28,27 @@ export const signIn = ash(async (req, res) => {
     res.status(200).json({ result: existingUser, token }); // User is successfully logged in
 });
 
+// Disable new user signups
 export const signUp = ash(async (req, res) => {
-    const { firstName, lastName, email, password, confirmPassword } = req.body; // Destructure data from req.body
 
-    // Fixing user input to ensure first & last name begin with capital letters
-    const fName = firstName.substr(0, 1).toUpperCase() + firstName.substr(1);
-    const lName = lastName.substr(0, 1).toUpperCase() + lastName.substr(1);
-
-    const existingUser = await User.findOne({ email }); // Query database to check if email already exists
-    if (existingUser) return res.status(400).json({ message: "That email is already registered" }); // If so, send error
-    if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" }); // Password correctness check
-
-    const hashedPassword = await bcrypt.hash(password, 12); // Take entered password and create hash using bcrypt
-
-    const newUser = await User.create({ email, password: hashedPassword, name: `${fName} ${lName}` }); // Add new user to database
-
-    const token = jwt.sign({ email: newUser.email, id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" }); // JWT_SECRET can be set in .env file
-
-    res.status(200).json({ result: newUser, token }); // User was successfully signed up
 });
+
+//export const signUp = ash(async (req, res) => {
+//    const { firstName, lastName, email, password, confirmPassword } = req.body; // Destructure data from req.body
+
+//    // Fixing user input to ensure first & last name begin with capital letters
+//    const fName = firstName.substr(0, 1).toUpperCase() + firstName.substr(1);
+//    const lName = lastName.substr(0, 1).toUpperCase() + lastName.substr(1);
+
+//    const existingUser = await User.findOne({ email }); // Query database to check if email already exists
+//    if (existingUser) return res.status(400).json({ message: "That email is already registered" }); // If so, send error
+//    if (password !== confirmPassword) return res.status(400).json({ message: "Passwords don't match" }); // Password correctness check
+
+//    const hashedPassword = await bcrypt.hash(password, 12); // Take entered password and create hash using bcrypt
+
+//    const newUser = await User.create({ email, password: hashedPassword, name: `${fName} ${lName}` }); // Add new user to database
+
+//    const token = jwt.sign({ email: newUser.email, id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" }); // JWT_SECRET can be set in .env file
+
+//    res.status(200).json({ result: newUser, token }); // User was successfully signed up
+//});
